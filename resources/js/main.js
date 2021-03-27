5c72632d63a98a1d4c8c9de5c48fb85f70db2a2a
Alexnhlst <agostinacchioalessandro@gmail.com>
2021-03-27 18:10:15 +0100

    Fixed HTML&CSS structure, added JS functionality for skill bars, container selector

--- resources/js/main.js
+++ resources/js/main.js
@@ -4,7 +4,6 @@ let text = element.innerHTML;
 let cursor = document.createElement("div");
 let wrap = document.querySelector(".wrap");
 cursor.className = "cursor";
-// cursor.innerHTML = "|";
 
 const timer = (ms) => new Promise((res) => setTimeout(res, ms));
 setTimeout(() => {
@@ -30,15 +29,61 @@ setTimeout(() => {
   addText();
 }, 6000);
 
-let about = document.getElementById("about");
-let project = document.getElementById("project");
-console.log(about);
+const container = document.querySelector(".container");
 
 function scrollFunction() {
   if (window.scrollY > 500) {
-    about.classList.add("slideup");
-    project.classList.add("slideup");
+    container.classList.add("slideup");
   }
 }
 
 window.onscroll = scrollFunction;
+
+// show progress bars
+let info = document.querySelector(".info");
+skill = "";
+
+let Skills = {
+  skillName: ["HTML", "CSS", "JS", "Java", "React"],
+  skillPercent: ["90%", "80%", "60%", "40%", "30%"],
+};
+
+for (let i = 0; i < 5; i++) {
+  skill += `
+            <span>${Skills.skillName[i]}</span>
+              <div class="bar">
+                <div class="progressLine" style="width: ${Skills.skillPercent[i]}">
+                  <span></span>
+                  <div class="balloon"><span>${Skills.skillPercent[i]}</span></div>
+                </div>
+              </div>`;
+}
+
+info.innerHTML = skill;
+
+let aboutSelector = document.getElementById("about");
+
+let skillSelector = document.getElementById("skills");
+console.log(skillSelector);
+
+let about = document.querySelector(".about");
+console.log(about);
+let skills = document.querySelector(".skills");
+console.log(skills);
+
+let hidden = document.querySelector(".hidden");
+console.log(hidden);
+
+skillSelector.addEventListener("click", () => {
+  about.classList.add("hidden");
+  aboutSelector.classList.add("text-not-selected");
+  skillSelector.classList.remove("text-not-selected");
+  skills.classList.remove("hidden");
+});
+
+aboutSelector.addEventListener("click", () => {
+  skills.classList.add("hidden");
+  skillSelector.classList.add("text-not-selected");
+  aboutSelector.classList.remove("text-not-selected");
+  about.classList.remove("hidden");
+});
